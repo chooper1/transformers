8fc6b47f67f1d04173d0227caa4d77db2482f34a
chooper1 <54963573+chooper1@users.noreply.github.com>
2023-02-02 13:47:50 -0800

    Create run_translation.sh

---
+++ examples/pytorch/translation/run_translation.sh
@@ -0,0 +1,15 @@
+export CUDA_VISIBLE_DEVICES=1
+python run_translation.py \
+    --model_name_or_path t5-small \
+    --do_eval \
+    --source_lang en \
+    --target_lang de \
+    --dataset_name iwslt2017 \
+    --dataset_config_name iwslt2017-de-en \
+    --output_dir /scratch/chooper/iwslt/tst-translation2 \
+    --source_prefix 'translate English to German: ' \
+    --per_device_train_batch_size=32 \
+    --per_device_eval_batch_size=32 \
+    --overwrite_output_dir \
+    --cache_dir /scratch/chooper/iwslt/cache-translation2 \
+    --predict_with_generate
